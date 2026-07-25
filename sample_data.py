3c916370def0626b1c32657c2199928f5fb36cb5
avroramar <avroramar@ukr.net>
2026-07-25 19:32:13 +0300

    Add sample product data to sample_data.py
    Added sample product data including name, quantity, and price for various items.

---
+++ sample_data.py
@@ -0,0 +1,27 @@
+PRODUCTS = [
+
+    {
+        "name": "Laptop",
+        "quantity": 6,
+        "price": 850
+    },
+
+    {
+        "name": "Keyboard",
+        "quantity": 18,
+        "price": 35
+    },
+
+    {
+        "name": "Mouse",
+        "quantity": 9,
+        "price": 22
+    },
+
+    {
+        "name": "Monitor",
+        "quantity": 12,
+        "price": 260
+    }
+
+]
